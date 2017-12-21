db536c79e4b8e5a50ab545dbfc7eec7a57d89800
J.D. Bean <jonathandbean@gmail.com>
2017-12-21 23:23:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == " " #return false if index is empty
     return false
+  elseif board[index] == nil || board[index] == "" #return false if index is empty (edge case)
+    return false
+  elseif board[index] == "X" || board[index] == "O" #return true if index contains player "mark"
+    return true
   end
 end
