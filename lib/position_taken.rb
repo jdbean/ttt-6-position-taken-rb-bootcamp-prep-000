8218c89853bdb4c15115796d95eb65029d6c25a1
J.D. Bean <jonathandbean@gmail.com>
2017-12-21 23:23:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " #return false if index is empty
     return false
-  elseif board[index] == nil || board[index] == "" #return false if index is empty (edge case)
+  elseif (board[index] == nil) || (board[index] == "") #return false if index is empty (edge case)
     return false
   elseif board[index] == "X" || board[index] == "O" #return true if index contains player "mark"
     return true
